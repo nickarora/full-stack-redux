c5be3481c4a97dd7e3366ca8f6e9e2becf69c8cf
Nick Arora <arora.nick@gmail.com>
2016-01-21 18:20:23 -0800

    remove dependencies and view engine

--- app.js
+++ app.js
@@ -1,60 +1,35 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var Express = require('express');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var app = Express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(Express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.get('/', handleRender);
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function handleRender(req, res) {
+  res.send(renderFullPage());
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+function renderFullPage() {
+  return (
+      '<!doctype html>' +
+      '<html>' +
+        '<head>' +
+          '<title>Redux Rethink</title>' +
+          "<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,200' rel='stylesheet' type='text/css'>" +
+          "<link href='/stylesheets/style.css' rel='stylesheet' type='text/css'>" +
+        '</head>' +
+        '<body>' +
+          '<h3>Hello World</h3>' +
+          '<div id="app"></div>' +
+        '</body>' +
+      '</html>'
+  );
+}
 
 module.exports = app;
