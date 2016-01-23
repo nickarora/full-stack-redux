a47a70e66b16917c311a4d30d8997ec8dc65ce73
Nick Arora <arora.nick@gmail.com>
2016-01-22 23:28:26 -0800

    client redux rendering after serve-side rendering

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
-import { Route, match, RouterContext } from 'react-router';
+import { match, RouterContext } from 'react-router';
 import { syncHistory } from 'redux-simple-router';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -13,23 +13,23 @@ import routes from './routes';
 
 export function handleRender(req, res) {
 
+  const initialState = { todos: ['take out trash', 'buy milk'] };
+
+  const history = createHistory();
+  const simpleRouter = syncHistory(history);
+  const createStoreWithMiddleware = applyMiddleware(thunk, simpleRouter)(createStore);
+  const store = createStoreWithMiddleware(reducers, initialState);
+
+  simpleRouter.listenForReplays(store);
+
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-
-    const initialState = { todos: ['take out trash', 'buy milk'] };
-
-    const history = createHistory();
-    const simpleRouter = syncHistory(history);
-    const createStoreWithMiddleware = applyMiddleware(thunk, simpleRouter)(createStore);
-    const store = createStoreWithMiddleware(reducers, initialState);
-
-    // Eventually will help us when we install redux devtools
-    simpleRouter.listenForReplays(store);
 
     if (error) {
       res.status(500).send(error.message)
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
+
       const html = ReactDOMServer.renderToString(
         <div>
           <Provider store={store}>
@@ -37,11 +37,11 @@ export function handleRender(req, res) {
           </Provider>
         </div>
       );
-      res.render('index', { html: html, initialState: JSON.stringify(store.getState())});
+
+      res.render('index', { html: html, initialState: JSON.stringify(store.getState())} );
     } else {
       res.status(404).render('error');
     }
-
   });
 
 }
--- src/client.js
+++ src/client.js
@@ -1,7 +1,47 @@
-/**
- * Starting point for webpack bundler
- */
+import ReactDOM from 'react-dom';
+import React from 'react';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { syncHistory } from 'redux-simple-router';
+
+import { browserHistory } from 'react-router';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+import routes from '../server/routes';
+import reducers from './reducers';
 
 import '../style/pure.css';
 import '../style/main.css';
+
+/* require components */
+import App from '../src/containers/App';
+
+let initialState = window.__INITIAL_STATE__;
+
+const logger = createLogger({
+  level: 'info',
+  collapsed: true
+});
+
+const simpleRouter = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(thunk, simpleRouter, logger)(createStore);
+const store = createStoreWithMiddleware(reducers, initialState);
+
+simpleRouter.listenForReplays(store);
+
+const domEl = document.getElementById('app');
+if (domEl) {
+  ReactDOM.render(
+    <div>
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route path="/" component={App}></Route>
+        </Router>
+      </Provider>
+    </div>,
+    domEl
+  );
+}
