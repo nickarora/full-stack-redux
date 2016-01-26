41c6802b04a1fa440de6a44d8fb358bc54e5b136
Nick Arora <arora.nick@gmail.com>
2016-01-26 11:21:25 -0800

    update description of server-side test

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -7,7 +7,7 @@ import server from '../../server.babel';
 const endpoint = `${express.host}:${express.appPort}/api`;
 
 describe('Todos Service', () => {
-  it ('should return all todos on fetch', done => {
+  it ('should return all todos on GET api/todos', done => {
     request.get(`${endpoint}/todos`).end((err, res) => {
       expect(err).to.be.null;
       expect(res.body).to.be.an.array;
