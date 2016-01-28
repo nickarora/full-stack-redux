dff527527283e57c290d3d89742557adde9b503d
Nick Arora <arora.nick@gmail.com>
2016-01-27 19:08:24 -0800

    use before and after hooks to populate test db

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -2,16 +2,34 @@ import request from 'superagent';
 import { expect } from 'chai';
 
 import { express } from '../../config/default.json';
+import seedTodos from './test_seed';
+import Todo from '../../server/api/models/todo';
+
 import server from '../../server.babel';
 
 const endpoint = `${express.host}:${express.appPort}/api`;
 
 describe('Todos Service', () => {
 
+  Todo.collection.drop();
+
+  beforeEach((done) => {
+    Todo.collection.insert(seedTodos, (err, docs) => {
+      if (err) throw err;
+      done();
+    })
+  });
+
+  afterEach((done) => {
+    Todo.collection.drop();
+    done();
+  });
+
   it ('should return all todos on GET api/todos', done => {
     request.get(`${endpoint}/todos`).end((err, res) => {
       expect(err).to.be.null;
       expect(res.body).to.be.an.array;
+      expect(res.body).to.have.length(4);
       res.body.forEach(todo => expect(todo.note).to.exist);
       done();
     });
---
+++ test/server/test_seed.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    note: 'Todo1',
+    completed: false,
+    created_at: Date.now()
+  },
+  {
+    note: 'Todo2',
+    completed: false,
+    created_at: Date.now()
+  },
+  {
+    note: 'Todo3',
+    completed: true,
+    created_at: Date.now()
+  },
+  {
+    note: 'Todo4',
+    completed: false,
+    created_at: Date.now()
+  },
+];
