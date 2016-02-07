7b28136fad1d97fa09090ed5c96d93b82b1ae03c
Nick Arora <arora.nick@gmail.com>
2016-02-06 21:28:42 -0800

    add checkbox to mark completion

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,13 @@ import React, { PropTypes } from 'react'
 const Todo = (props) => {
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={ props.completed}
+        onClick={props.onClick}
+        />
       <div
         className="todo"
-        onClick={props.onClick}
         style={{
             textDecoration: props.completed ? 'line-through' : 'none',
             cursor: props.completed ? 'default' : 'pointer'
