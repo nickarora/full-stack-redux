1462c0465696c84cde5bb98d70fe8e2d706ab8dc
Nick Arora <arora.nick@gmail.com>
2016-02-10 03:04:03 -0800

    httpS in production

--- src/actions/endpoint.js
+++ src/actions/endpoint.js
@@ -4,7 +4,7 @@ let endpoint = `http://${express.host}:${express.devServerPort}/api`;
 
 if (process.env.NODE_ENV === 'production') {
   const port = process.env.PORT || express.appPort;
-  endpoint = `http://${express.host}:${port}/api`;
+  endpoint = `https://${express.host}:${port}/api`;
 }
 
 export default endpoint;
