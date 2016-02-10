1594001938e919b3e06136027ac00234f88b3e98
Nick Arora <arora.nick@gmail.com>
2016-02-10 02:37:06 -0800

    db connection logging

--- server/db_connection.js
+++ server/db_connection.js
@@ -13,4 +13,9 @@ switch (process.env.NODE_ENV) {
     dbType = `mongodb://${config.mongo.host}/${config.mongo.dev}`;
 }
 
+
+console.log('my node env: ' + process.env.NODE_ENV);
+console.log('my mongo uri: ' + process.env.MONGOLAB_URI);
+console.log('SELECTED DB: ' + dbType);
+
 export default dbType;
