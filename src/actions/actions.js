dccc843fe222b8c4b6f45d8c5aa5c45cb901d429
Nick Arora <arora.nick@gmail.com>
2016-02-16 20:00:57 -0800

    refactor actions.js to adhere to style guide

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,44 +3,55 @@ import endpoint from './endpoint';
 
 import randomHex from '../util/random_hex';
 
-import * as types from '../constants/actionTypes'
+import * as types from '../constants/actionTypes';
 
-export const toggleTodo = (todo) => {
+const requestToggleTodo = (todo) => ({ type: types.REQUEST_TODO_TOGGLE, todo });
+const todoToggleSuccess = (todo) => ({ type: types.TODO_TOGGLE_SUCCESS, todo });
+const todoToggleFail = (todo) => ({ type: types.TODO_TOGGLE_FAIL, todo });
 
-  return function(dispatch) {
-
+export const toggleTodo = (todo) =>
+  (dispatch) => {
     dispatch(requestToggleTodo(todo));
     const update = { completed: !todo.completed };
+
     request
       .put(`${endpoint}/todos/${todo._id}`)
       .send(update)
-      .end( (err, { body }) => {
-        if (err)
-          dispatch(todoToggleFail(todo))
-        else
-          dispatch(todoToggleSuccess(body))
+      .end((err, { body }) => {
+        if (err) {
+          dispatch(todoToggleFail(todo));
+        } else {
+          dispatch(todoToggleSuccess(body));
+        }
       });
-  }
-};
+  };
 
-export const deleteTodo = (todo) => {
-  return function(dispatch) {
+const requestDelTodo = (todo) => ({ type: types.REQUEST_DEL_TODO, todo });
+const delTodoSuccess = () => ({ type: types.DEL_TODO_SUCCESS });
+const delTodoFail = (todo) => ({ type: types.DEL_TODO_FAIL, todo });
+
+export const deleteTodo = (todo) =>
+  (dispatch) => {
     dispatch(requestDelTodo(todo));
 
     request
       .del(`${endpoint}/todos/${todo._id}`)
-      .end( (err, { body }) => {
-        if (err)
-          dispatch(delTodoFail(todo))
-        else
-          dispatch(delTodoSuccess())
+      .end((err) => {
+        if (err) {
+          dispatch(delTodoFail(todo));
+        } else {
+          dispatch(delTodoSuccess());
+        }
       });
-  }
-};
+  };
 
-export const addTodo = (inputText) => {
-  return function(dispatch) {
-    if (inputText == "") { return }
+const requestAddTodo = (todo) => ({ type: types.REQUEST_ADD_TODO, todo });
+const addTodoSuccess = (todo) => ({ type: types.ADD_TODO_SUCCESS, todo });
+const addTodoFail = (todo) => ({ type: types.ADD_TODO_FAIL, todo });
+
+export const addTodo = (inputText) =>
+  (dispatch) => {
+    if (inputText === '') { return; }
 
     const todo = { _id: randomHex(), note: inputText, completed: false, created_at: 'pending' };
 
@@ -49,61 +60,13 @@ export const addTodo = (inputText) => {
     request
       .post(`${endpoint}/todos`)
       .send({ _id: todo._id, note: inputText })
-      .end( (err, { body }) => {
-        if (err)
-          dispatch(addTodoFail(todo))
-        else
-          dispatch(addTodoSuccess(body))
+      .end((err, { body }) => {
+        if (err) {
+          dispatch(addTodoFail(todo));
+        } else {
+          dispatch(addTodoSuccess(body));
+        }
       });
-  }
-}
+  };
 
-export const inputChange = (inputText) => {
-
-  return {
-    type: types.TEXT_INPUT_CHANGE,
-    inputText
-  }
-};
-
-const requestAddTodo = (todo) => {
-  return {
-    type: types.REQUEST_ADD_TODO,
-    todo
-  }
-};
-
-const addTodoSuccess = (todo) => {
-  return { type: types.ADD_TODO_SUCCESS, todo }
-};
-
-const addTodoFail = (todo) => {
-  return { type: types.ADD_TODO_FAIL, todo }
-};
-
-const requestDelTodo = (todo) => {
-  return {
-    type: types.REQUEST_DEL_TODO,
-    todo
-  }
-};
-
-const delTodoSuccess = () => {
-  return { type: types.DEL_TODO_SUCCESS }
-};
-
-const delTodoFail = (todo) => {
-  return { type: types.DEL_TODO_FAIL, todo }
-};
-
-const requestToggleTodo = (todo) => {
-  return { type: types.REQUEST_TODO_TOGGLE, todo}
-};
-
-const todoToggleSuccess = (todo) => {
-  return { type: types.TODO_TOGGLE_SUCCESS, todo }
-};
-
-const todoToggleFail = (todo) => {
-  return { type: types.TODO_TOGGLE_FAIL, todo }
-};
+export const inputChange = (inputText) => ({ type: types.TEXT_INPUT_CHANGE, inputText });
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,28 +1,27 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 
-const TodoInput = (props) => {
-  return (
-    <form className="todo-input-form" onSubmit = { (e) => {
-        e.preventDefault();
-        const todo = {};
-        props.onSubmit(todo);
-      }}>
+const handleSubmit = (props) => (e) => {
+  e.preventDefault();
+  props.onSubmit();
+};
+
+const TodoInput = (props) =>
+    <form className="todo-input-form" onSubmit={handleSubmit(props)}>
       <div className="todo-input-form-group">
       <input
         type="text"
         placeholder="Today I need to..."
-        value={ props.todoText }
-        onChange={ props.onInputChange }/>
+        value={props.todoText}
+        onChange={props.onInputChange}
+      />
       <button type="submit">+</button>
       </div>
-    </form>
-  )
-}
+    </form>;
 
 TodoInput.propTypes = {
   todoText: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
   onInputChange: PropTypes.func.isRequired
-}
+};
 
 export default TodoInput;
