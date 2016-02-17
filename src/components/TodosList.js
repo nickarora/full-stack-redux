3a4df6fd93376a46b1448e4fafa65d2c8fbcdefe
Nick Arora <arora.nick@gmail.com>
2016-02-16 19:36:57 -0800

    refactor TodosList.js to adhere to linter

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,30 +1,32 @@
 import React, { PropTypes } from 'react';
 
-import Todo from './Todo'
+import Todo from './Todo';
 
-const TodosList = (props) => {
-  return (
+const handleTodoDelete = (props, todo) => () => props.onTodoDelete(todo);
+const handleTodoClick = (props, todo) => () => props.onTodoClick(todo);
+
+const TodosList = (props) =>
     <ul className="todos-list">
       {props.todos.map(todo =>
-        <Todo key={todo._id}
-              note={todo.note}
-              completed={todo.completed}
-              onDelete={() => props.onTodoDelete(todo)}
-              onClick={() => props.onTodoClick(todo)} />
+        <Todo
+          key={todo._id}
+          note={todo.note}
+          completed={todo.completed}
+          onDelete={handleTodoDelete(props, todo)}
+          onClick={handleTodoClick(props, todo)}
+        />
       )}
-    </ul>
-  )
-}
+    </ul>;
 
 const todosShape = PropTypes.shape({
   note: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired
-}).isRequired
+}).isRequired;
 
 TodosList.propTypes = {
   onTodoDelete: PropTypes.func.isRequired,
   onTodoClick: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(todosShape).isRequired
-}
+};
 
 export default TodosList;
