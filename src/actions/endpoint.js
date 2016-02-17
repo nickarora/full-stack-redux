5e5f46c946637003317d17258efe6958e077f0f6
Nick Arora <arora.nick@gmail.com>
2016-02-16 20:02:57 -0800

    refactor endpoint.js to adhere to styleguide

--- src/actions/endpoint.js
+++ src/actions/endpoint.js
@@ -3,7 +3,6 @@ import { express } from '../../config/default.json';
 let endpoint = `http://${express.host}:${express.devServerPort}/api`;
 
 if (process.env.NODE_ENV === 'production') {
-  const port = process.env.PORT || express.appPort;
   endpoint = `https://${express.prodHost}/api`;
 }
 
