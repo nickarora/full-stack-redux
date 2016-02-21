e31c35c3dc10894d85afea49d4c1125ada806435
Nick Arora <arora.nick@gmail.com>
2016-02-20 22:05:45 -0800

    finish specs for todo component

--- test/client/components/todo.test.js
+++ test/client/components/todo.test.js
@@ -9,18 +9,33 @@ import Todo from '../../../src/components/Todo';
 chai.use(chaiEnzyme());
 
 describe('<Todo />', () => {
-  const onTodoClick = sinon.spy();
-  const onDeleteClick = sinon.spy();
+  const toggleTodo = sinon.spy();
+  const deleteTodo = sinon.spy();
+
+  const wrapper = shallow(
+    <Todo
+      onClick={ toggleTodo }
+      onDelete={ deleteTodo }
+      note={'My Todo'}
+      completed={false}
+    />
+  );
 
   it('should have the className todo', () => {
-    const wrapper = shallow(
-      <Todo
-        onClick={onTodoClick}
-        onDelete={onDeleteClick}
-        note={'My Todo'}
-        completed={false}
-      />
-    );
     expect(wrapper).to.have.className('todo');
+  });
+
+  it('should display the text of the todo', () => {
+    expect(wrapper.text()).to.contain('My Todo');
+  });
+
+  it('should call toggleTodo action creator on checkbox click', () => {
+    wrapper.find('input[type="checkbox"]').simulate('click');
+    expect(toggleTodo).to.be.calledOnce;
+  });
+
+  it('should call deleteTodo action creator on delete button click', () => {
+    wrapper.find('button').simulate('click');
+    expect(deleteTodo).to.be.calledOnce;
   });
 });
