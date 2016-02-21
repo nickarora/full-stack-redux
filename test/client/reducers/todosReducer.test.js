3798e85382ee57ddc535b389882adb927861f99d
Nick Arora <arora.nick@gmail.com>
2016-02-20 20:40:31 -0800

    update todosReducers to adhere to linter

--- test/client/reducers/todosReducer.test.js
+++ test/client/reducers/todosReducer.test.js
@@ -1,28 +1,27 @@
-import { expect } from 'chai'
-import todosReducer from '../../../src/reducers/todos'
+import { expect } from 'chai';
+import todosReducer from '../../../src/reducers/todos';
 
-import * as types from '../../../src/constants/actionTypes'
+import * as types from '../../../src/constants/actionTypes';
 
 describe('todos reducer', () => {
-
-  let state = [{
+  const state = [{
     _id: 0,
     note: 'Use Redux',
     completed: false,
-    created_at: "2016"
+    created_at: '2016'
   }, {
     _id: 1,
     note: 'Dont use Flux',
     completed: false,
-    created_at: "2016"
-  }]
+    created_at: '2016'
+  }];
 
   it('should return the initial state', () => {
     expect(
       todosReducer(state, {})
     ).to.eql(
       state
-    )
+    );
   });
 
   it('should handle REQUEST_TOGGLE_TODO', () => {
@@ -33,53 +32,50 @@ describe('todos reducer', () => {
       })
     ).to.eql([
       state[0], { ...state[1], completed: !state[1].completed }
-    ])
+    ]);
   });
 
   it('should handle REQUEST_ADD_TODO', () => {
-    const todo = {_id: 'X123', note: 'note', completed: false, created_at: 'pending'};
+    const todo = { _id: 'X123', note: 'note', completed: false, created_at: 'pending' };
 
     expect(
       todosReducer(state, {
         type: types.REQUEST_ADD_TODO,
         todo
       })
-    ).to.eql( state.concat(todo) )
-  })
+    ).to.eql(state.concat(todo));
+  });
 
   it('should handle ADD_TODO_FAIL', () => {
-    const todo = {_id: 'X123', note: 'note', completed: false, created_at: 'pending'};
+    const todo = { _id: 'X123', note: 'note', completed: false, created_at: 'pending' };
 
     expect(
       todosReducer(state.concat(todo), {
         type: types.ADD_TODO_FAIL,
         todo
       })
-    ).to.eql( state )
+    ).to.eql(state);
   });
 
   it('should handle REQUEST_DEL_TODO', () => {
-    const todo = state[1]
+    const todo = state[1];
 
     expect(
       todosReducer(state, {
         type: types.REQUEST_DEL_TODO,
         todo
       })
-    ).to.eql( [state[0]] )
-  })
+    ).to.eql([state[0]]);
+  });
 
   it('should handle DEL_TODO_FAIL', () => {
-    const todo = state[1]
+    const todo = state[1];
 
     expect(
-      todosReducer([ state[0] ], {
+      todosReducer([state[0]], {
         type: types.DEL_TODO_FAIL,
         todo
       })
-    ).to.eql( state )
-  })
-
+    ).to.eql(state);
+  });
 });
-
-
