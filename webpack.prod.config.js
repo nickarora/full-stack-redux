a120f7cdb0ce6a242ca75dacd578eb9cf4339df5
Nick Arora <arora.nick@gmail.com>
2016-02-21 20:29:45 -0800

    update webpack.prod.config.js to adhere to linter

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,37 +8,37 @@ module.exports = {
   target: 'web',
 
   entry: [
-      './src/client'
+    './src/client'
   ],
 
   output: {
-      path: path.join(__dirname, './dist'),
-      filename: './bundle.js',
-      publicPath: '/dist/'
+    path: path.join(__dirname, './dist'),
+    filename: './bundle.js',
+    publicPath: '/dist/'
   },
 
   module: {
-      loaders: [{
-          test: /\.js$/,
-          loaders: ['babel'],
-          exclude: /node_modules/,
-          include: __dirname
-      }, {
-          test: /\.scss?$/,
-          loaders: ['style', 'css', 'postcss', 'sass'],
-          include: __dirname
-      }, {
-          test: /\.json$/,
-          loader: 'json'
-      }]
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }, {
+      test: /\.scss?$/,
+      loaders: ['style', 'css', 'postcss', 'sass'],
+      include: __dirname
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }]
   },
 
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'PORT': JSON.stringify(process.env.PORT)
-      },
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        PORT: JSON.stringify(process.env.PORT)
+      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
@@ -51,6 +51,6 @@ module.exports = {
   postcss: [autoprefixer],
 
   resolve: {
-      extensions: ['', '.jsx', '.js', '.webpack.js', '.scss' ]
+    extensions: ['', '.jsx', '.js', '.webpack.js', '.scss']
   }
-}
+};
