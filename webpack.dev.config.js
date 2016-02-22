42635de47cec12236a5d66814b9f03bd9cec6cd5
Nick Arora <arora.nick@gmail.com>
2016-02-21 20:09:36 -0800

    update webpack dev server to adhere to linter

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,5 @@
 const MochaTestRunner = require('./plugins/MochaTestRunner');
 
-const path = require('path');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const expressConfig = require('./config/default.json').express;
@@ -9,40 +8,40 @@ module.exports = {
   target: 'web',
 
   entry: [
-      'webpack-dev-server/client?http://' + expressConfig.host + ':' + expressConfig.devServerPort,
-      'webpack/hot/only-dev-server',
-      './src/client.js'
+    `webpack-dev-server/client?http://${expressConfig.host}:${expressConfig.devServerPort}`,
+    'webpack/hot/only-dev-server',
+    './src/client.js'
   ],
 
   output: {
-      path: __dirname + '/',
-      filename: 'bundle.js',
-      publicPath: 'http://' + expressConfig.host + ':' + expressConfig.devServerPort + "/"
+    path: `${__dirname}/`,
+    filename: 'bundle.js',
+    publicPath: `http://${expressConfig.host}:${expressConfig.devServerPort}/`
   },
 
   module: {
-      loaders: [{
-          test: /\.js$/,
-          loaders: ['react-hot', 'babel', 'eslint'],
-          exclude: /node_modules/,
-          include: __dirname
-      }, {
-          test: /\.scss?$/,
-          loaders: ['style', 'css', 'postcss', 'sass'],
-          include: __dirname
-      }, {
-          test: /\.json$/,
-          loader: 'json'
-      }]
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel', 'eslint'],
+      exclude: /node_modules/,
+      include: __dirname
+    }, {
+      test: /\.scss?$/,
+      loaders: ['style', 'css', 'postcss', 'sass'],
+      include: __dirname
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }]
   },
 
   plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }),
-      new webpack.NoErrorsPlugin(),
-      new MochaTestRunner()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.NoErrorsPlugin(),
+    new MochaTestRunner()
   ],
 
   eslint: {
@@ -52,8 +51,8 @@ module.exports = {
   postcss: [autoprefixer],
 
   resolve: {
-      extensions: ['', '.jsx', '.js', '.webpack.js', '.scss' ]
+    extensions: ['', '.jsx', '.js', '.webpack.js', '.scss']
   },
 
   devtool: 'source-map'
-}
+};
