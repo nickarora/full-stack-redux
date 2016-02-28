ec8f4d35e8682c98b714c148f88f0990e6bbd01b
Nick Arora <arora.nick@gmail.com>
2016-02-27 21:16:43 -0800

    update css to work on mobile

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -8,14 +8,14 @@ const handleSubmit = (props) => (e) => {
 const TodoInput = (props) =>
     <form className="todo-input-form" onSubmit={handleSubmit(props)}>
       <div className="todo-input-form-group">
-      <input
-        type="text"
-        placeholder="Today I need to..."
-        value={props.todoText}
-        onChange={props.onInputChange}
-      />
-      <button type="submit">+</button>
+        <input
+          type="text"
+          placeholder="Today I need to..."
+          value={props.todoText}
+          onChange={props.onInputChange}
+        />
       </div>
+      <button type="submit"><div className="todo-button-text">+</div></button>
     </form>;
 
 TodoInput.propTypes = {
